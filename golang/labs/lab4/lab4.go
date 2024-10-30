5a7f1afebc2fef4dd721eae1f930f71775c4efb1
taaaye <ilia.kuzminov.04@mail.ru>
2024-10-30 17:14:56 +0300

    new changes lab4

--- golang/labs/lab4/lab4.go
+++ golang/labs/lab4/lab4.go
@@ -1,34 +1,37 @@
 package labs
 //9 вариант
 import (
-	"fmt"
-	"math"
+  "fmt"
+  "math"
 )
 
-func calculateY(x float64) float64 {
-	var a float64 = 1.1
-	var b float64 = 0.09
-	numerator := math.Log10(x*x - 1)
-	denominator := math.Log(a*x*x-b) / math.Log(5)
-	return numerator / denominator
+func CalculateY(x, a, b float64) float64 {
+  numerator := math.Log10(x*x - 1)
+  denominator := math.Log(a*x*x-b) / math.Log(5)
+  y := numerator / denominator
+  return y
 }
-func Laba4A() []float64 {
-	fmt.Print("Задача A\n")
-	var x_a float64 = 1.2
-	results := []float64{}
-	for i := 1.2; i < 2.2; i += 0.2 {
-		y := calculateY(x_a)
-		results = append(results, y)
-		x_a += 0.2
-	}
-	return results
+
+func TaskA(xn, xk, xdelta, a, b float64) []float64 {
+  var y_a = []float64{}
+  for i := xn; i < xk; i = i + xdelta {
+    y_a = append(y_a, CalculateY(i, a, b))
+  }
+  return y_a
 }
-func Laba4B() (float64) {
-	fmt.Print("Задача B\n")
-	xn := [5]float64{1.21, 1.76, 2.53, 3.48, 4.52}
-	var x_b float64
-	for _, x_b := range xn {
-		fmt.Print(calculateY(x_b), "\n")
-	}
-	return x_b
+
+func TaskB(xs []float64, a, b float64) []float64 {
+  var y_b = []float64{}
+  for _, x_b := range xs {
+    y_b = append(y_b, CalculateY(x_b, a, b))
+  }
+  return y_b  
+}
+
+func Laba4run() {
+  fmt.Print("Задача A\n")
+  fmt.Print(TaskA(1.2, 2.2, 0.2, 1.1, 0.09), "\n")
+  fmt.Print("Задача B\n")
+  var x = []float64{1.21, 1.76, 2.53, 3.48, 4.52}
+  fmt.Print(TaskB(x, 1.1, 0.09), "\n")
 }
--- golang/main.go
+++ golang/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-		"fmt"
-
-		labs "isuct.ru/informatics2022/labs/lab4"
+    "fmt"
+    
+    lab4 "isuct.ru/informatics2022/labs/lab4"
 )
 func main() {
-	fmt.Println("Кузьминов Илья Владимирович")
-	results := labs.Laba4A()
-	for _, result := range results {
-		fmt.Println(result)
-	}
-	labs.Laba4B()
+  fmt.Println("Кузьминов Илья Владимирович")
+  lab4.Laba4run()
 }
-
-
