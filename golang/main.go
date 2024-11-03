f989ac495ffc0b827a71919480f46d152b8341e7
taaaye <ilia.kuzminov.04@mail.ru>
2024-11-03 18:45:32 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
     "fmt"
-  
+    
+    lab4 "isuct.ru/informatics2022/labs/lab4"
+    
     lab6 "isuct.ru/informatics2022/labs/lab6"
 )
+
 func main() {
   fmt.Println("Кузьминов Илья Владимирович")
+  lab4.Laba4run()
   lab6.RunLab6()
 }
-
