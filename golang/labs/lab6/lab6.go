5c30a1055e9e3733e1429a6a4d8d22b807e9aec2
taaaye <ilia.kuzminov.04@mail.ru>
2024-11-09 13:15:10 +0300

    remastered

--- golang/labs/lab6/lab6.go
+++ golang/labs/lab6/lab6.go
@@ -4,77 +4,70 @@ import (
 	"os"
 )
 
+var Routes = map[int]string{
+    1: "Tokyo-Moscow",
+    2: "Moscow-Tokyo",
+    3: "Moscow-Dubai",
+    4: "Seoul-Moscow",
+    5: "Moscow-Seoul",
+    6: "Dubai-Moscow",
+}
+
 type Airplane struct {
-    name            string
-    flight_number   int
-    power           int
-    purchased_seats int
+    Name            string
+    FlightNumber   int
+    Power           int
+    PurchasedSeats int
 }
 
 func GetAirplaneName() string {
-    var name string
+    var Name string
     fmt.Print("Введите имя самолета: ")
-    fmt.Fscan(os.Stdin, &name)
-    return name
+    fmt.Fscan(os.Stdin, &Name)
+    return Name
 }
 
-func (a Airplane) speed_calculation(air_res int) int {
-    speed := a.power / air_res
-    return speed
+func (a Airplane) SpeedCalculation(AirRes int) int {
+    Speed := a.Power / AirRes
+    return Speed
 }
 
-func (a Airplane) revenue_calculation(price_for_place int) int {
-    money := price_for_place * a.purchased_seats
-    return money
+func (a Airplane) RevenueCalculation(PriceForPlace int) int {
+    Money := PriceForPlace * a.PurchasedSeats
+    return Money
 }
 
-func (a Airplane) get_route() string {
-    routes := map[int]string{
-        1: "Tokyo-Moscow",
-        2: "Moscow-Tokyo",
-        3: "Moscow-Dubai",
-        4: "Seoul-Moscow",
-        5: "Moscow-Seoul",
-        6: "Dubai-Moscow",
-    }
+func (a Airplane) GetRoute() string {
+    return Routes[a.FlightNumber]
 
-    return routes[a.flight_number]
 }
 
 func RunLab6() {
-    routes := map[int]string{
-        1: "Tokyo-Moscow",
-        2: "Moscow-Tokyo",
-        3: "Moscow-Dubai",
-        4: "Seoul-Moscow",
-        5: "Moscow-Seoul",
-        6: "Dubai-Moscow",
-    }
 
     fmt.Println("Выберите номер маршрута из списка:")
-    for num, route := range routes {
-        fmt.Printf("%d: %s\n", num, route)
+    for num, Route := range Routes {
+        fmt.Printf("%d: %s\n", num, Route)
     }
 
-    var route int
+    var Route int
     fmt.Print("Введите номер маршрута: ")
-    fmt.Scan(&route)
+    fmt.Scan(&Route)
 
-    var occupiedSeats int
+    var OccupiedSeats int
     fmt.Print("Введите количество занятых мест: ")
-    fmt.Scan(&occupiedSeats)
+    fmt.Scan(&OccupiedSeats)
 
     // Ввод имени самолета
     AirplaneName := GetAirplaneName()
 
     Plane := Airplane{
-        name:            AirplaneName,
-        flight_number:   route,
-        power:           480000,
-        purchased_seats: occupiedSeats,
+        Name:           AirplaneName,
+        FlightNumber:   Route,
+        Power:          480000,
+        PurchasedSeats: OccupiedSeats,
     }
 
-    fmt.Printf("%v направляется по маршруту %v\n", Plane.name, Plane.get_route())
-    fmt.Printf("Скорость %v равна %d м/с\n", Plane.name, Plane.speed_calculation(2000))
-    fmt.Printf("Прибыль с продажи билетов составит: %d рублей \n", Plane.revenue_calculation(40000))
+    fmt.Printf("%v направляется по маршруту %v\n", Plane.Name, Plane.GetRoute())
+    fmt.Printf("Скорость %v равна %d м/с\n", Plane.Name, Plane.SpeedCalculation(2000))
+    fmt.Printf("Прибыль с продажи билетов составит: %d рублей \n", Plane.RevenueCalculation(40000))
 }
