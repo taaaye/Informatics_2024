5b7ee86f0e1396ce2c14400ea081ec34ebc7fd20
taaaye <ilia.kuzminov.04@mail.ru>
2024-12-02 16:51:48 +0300

    lab6 structure Kuzminov (#316)
    * lab6 structure
    
    * Update main.go
    
    * remastered
    
    * remastered
    
    * lint-fix
    
    * changed

--- golang/main.go
+++ golang/main.go
@@ -4,8 +4,12 @@ import (
     "fmt"
     
     lab4 "isuct.ru/informatics2022/labs/lab4"
+    
+    lab6 "isuct.ru/informatics2022/labs/lab6"
 )
+
 func main() {
   fmt.Println("Кузьминов Илья Владимирович")
   lab4.Laba4run()
+  lab6.Laba6run()
 }
