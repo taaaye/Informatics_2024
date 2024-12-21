6dfc29c7dd8f9a122b4ee5050b0f07745644ce17
taaaye <ilia.kuzminov.04@mail.ru>
2024-12-21 13:25:01 +0300

    changed

--- golang/labs/lab7/electroics.go
+++ golang/labs/lab7/electroics.go
@@ -3,32 +3,32 @@ package lab7
 import "fmt"
 
 type Electronics struct {
-    name  string
-    price float64
-    brand string
-    color string
+	name  string
+	price float64
+	brand string
+	color string
 }
 
 func (e *Electronics) getPrice() float64 {
-    return e.price
+	return e.price
 }
 
 func (e *Electronics) setPrice(newPrice float64) {
-    e.price = newPrice
+	e.price = newPrice
 }
 
 func (e *Electronics) applyDiscount(discount float64) {
-    if discount < 0 || discount > 100 {
-        fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
-        return
-    }
-    e.price = e.price * (1 - discount/100)
+	if discount < 0 || discount > 100 {
+		fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
+		return
+	}
+	e.price = e.price * (1 - discount/100)
 }
 
 func (e *Electronics) getName() string {
-    return e.name
+	return e.name
 }
 
 func (e *Electronics) setName(newName string) {
-    e.name = newName
+	e.name = newName
 }
--- golang/labs/lab7/lab7.go
+++ golang/labs/lab7/lab7.go
@@ -3,42 +3,30 @@ package lab7
 import "fmt"
 
 func CalculationSumProduct(listproducts []Product) float64 {
-    var sum float64 = 0
-    for _, product := range listproducts {
-        sum += product.getPrice()
-    }
-    return sum
+	var sum float64 = 0
+	for _, product := range listproducts {
+		sum += product.getPrice()
+	}
+	return sum
 }
 
 func Laba7run() {
-    var tv Product = &Electronics{price: 24999.99, name: "Телевизор", brand: "Samsung", color: "Черный"}
-    var hoodys Product = &Hoody{price: 5000, name: "Толстовка Nikifilini", color: "Черный", size: "XXL"}
-    var trainers Product = &Shoes{price: 39000, name: "Air Jordan 3 'Black Cat'", brand: "Nike", size: "42"}
+	tv := &Electronics{price: 24999.99, name: "Телевизор", brand: "Samsung", color: "Черный"}
+	hoodys := &Hoody{price: 5000, name: "Толстовка Nikifilini", color: "Черный", size: "XXL"}
+	trainers := &Shoes{price: 39000, name: "Air Jordan 3 'Black Cat'", brand: "Nike", size: "42"}
 
-    tv.setPrice(29999.99)
-    tv.setName("Монитор")
-    
-    if h, ok := hoodys.(*Hoody); ok {
-        h.setColor("Белый")
-    } else {
-        fmt.Println("Ошибка приведения типа для Hoody")
-    }
+	tv.setPrice(29999.99)
+	tv.setName("Монитор")
+	trainers.setBrand("Puma")
 
-    if s, ok := trainers.(*Shoes); ok {
-        s.setName("RS-X Efekt S&P")
-        s.setBrand("Puma")
-    } else {
-        fmt.Println("Ошибка приведения типа для Shoes")
-    }
+	listproducts := []Product{tv, hoodys, trainers}
 
-    listproducts := []Product{tv, hoodys, trainers}
+	fmt.Printf("Сумма товаров, без учёта скидки, равна: %.2f рублей \n", CalculationSumProduct(listproducts))
 
-    fmt.Printf("Сумма товаров, без учёта скидки, равна: %.2f рублей \n", CalculationSumProduct(listproducts))
+	discounts := []float64{5, 25, 15}
+	for i, product := range listproducts {
+		product.applyDiscount(discounts[i])
+	}
 
-    discounts := []float64{5, 25, 15} // Скидки для каждого товара
-        for i, product := range listproducts {
-         product.applyDiscount(discounts[i])
- }
-
-    fmt.Printf("Сумма товаров, с учётом скидки, равна: %.2f рублей \n", CalculationSumProduct(listproducts))
+	fmt.Printf("Сумма товаров, с учётом скидки, равна: %.2f рублей \n", CalculationSumProduct(listproducts))
 }
