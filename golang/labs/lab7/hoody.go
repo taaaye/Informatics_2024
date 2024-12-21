0b9f732402c0a44c43a0a6ded9deecc37ea4f73d
taaaye <ilia.kuzminov.04@mail.ru>
2024-12-21 13:28:08 +0300

    lint fix

--- golang/labs/lab7/hoody.go
+++ golang/labs/lab7/hoody.go
@@ -3,35 +3,32 @@ package lab7
 import "fmt"
 
 type Hoody struct {
-    size  string
-    price float64
-    name  string
-    color string
+	size  string
+	price float64
+	name  string
+	color string
 }
+
 func (h *Hoody) getPrice() float64 {
-    return h.price
+	return h.price
 }
 
 func (h *Hoody) setPrice(newPrice float64) {
-    h.price = newPrice
+	h.price = newPrice
 }
 
 func (h *Hoody) applyDiscount(discount float64) {
-    if discount < 0 || discount > 100 {
-        fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
-        return
-    }
-    h.price = h.price * (1 - discount/100)
+	if discount < 0 || discount > 100 {
+		fmt.Println("Скидка должна быть в диапазоне от 0 до 100%")
+		return
+	}
+	h.price = h.price * (1 - discount/100)
 }
 
 func (h *Hoody) getName() string {
-    return h.name
+	return h.name
 }
 
 func (h *Hoody) setName(newName string) {
-    h.name = newName
-}
-
-func (h *Hoody) setColor(newColor string) {
-    h.color = newColor
+	h.name = newName
 }
